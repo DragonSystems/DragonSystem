e77395cbd2d4bdc38631c81bae2e43b242b28349
@kiyotocrypto <kiyotocrypto@gmail.com>
2017-08-29 15:39:08 +0800

    web pack update

---
+++ DragonSite/dragon.js
@@ -0,0 +1,19 @@
+// import util from 'ethereumjs-util'
+// import Web3 from 'web3'
+// import tx from 'ethereumjs-tx'
+// import time from 'moment'
+import key from 'keythereum'
+
+/**
+ * These will be accessabe as dragon.key
+ */
+export {
+  key,
+  // util,
+  // Web3,
+  // tx,
+  // time,
+}
+
+// You can add globals for each module istead like this:
+// window.key = key
